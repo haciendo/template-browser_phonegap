4f94525075df6f2fa6d4108afe3ca5c904be25f4
josetabuyo <josetabuyo@gmail.com>
2015-03-06 09:02:38 -0300

    JT: se restaura y se saca la carpeta VortexJS afuera, se deja solo el archivo vortex.min.js en el root

--- VortexJS/Script - UglifyJS.bat
+++
@@ -1,3 +0,0 @@
-echo minificando las cosas
-pause
-uglifyjs FiltrosYTransformaciones.js NodoClienteHTTP.js ClonadorDeObjetos.js GeneradorDeIdMensaje.js NodoPortalBidi.js NodoRouter.js PataConectora.js NodoConectorSocket.js Vortex.js -o vortex.min.js
